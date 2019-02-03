66e6245e9463355b22eb0afef0464ccc029b4833
Ania <jedruzko.anna.93@gmail.com>
2019-02-04 00:18:40 +0100

    add watch

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,10 @@ function hideTab(tabClassName) {
     var element = document.getElementsByClassName(tabClassName);
     element[0].style.display = 'none';
 }
+
+function date(){
+    var d = new Date();
+    var h = d.getHours() < 10 ? '0' + d.getHours() : d.getHours();
+    var m = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
+    document.getElementById("zegarek").innerHTML = h + ":" + m;
+  }
